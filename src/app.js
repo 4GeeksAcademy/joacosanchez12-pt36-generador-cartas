1443472827bdd2e78f6d9535ddef3bf423639448
joacosanchez12 <joaquin.sancheezz@gmail.com>
2024-10-01 01:37:36 +0000

    Generador de Cartas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const icons = [
+    { key: "♦", color: "red" },
+    { key: "♥", color: "red" },
+    { key: "♠", color: "black" },
+    { key: "♣", color: "black" }
+  ];
+
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  // Función para generar una carta aleatoria
+  function generarCarta() {
+    let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.querySelector(".top-suit").innerHTML = randomIcon.key;
+    document.querySelector(".top-suit").style.color = randomIcon.color;
+    document.querySelector(".bottom-suit").innerHTML = randomIcon.key;
+    document.querySelector(".bottom-suit").style.color = randomIcon.color;
+    document.querySelector(".number").innerHTML = randomNumber;
+  }
+
+  generarCarta();
+
+  // Asocia el evento al botón
+  document
+    .getElementById("generarCarta")
+    .addEventListener("click", generarCarta);
 };
